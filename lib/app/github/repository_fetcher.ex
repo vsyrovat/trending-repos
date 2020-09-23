a1c87c295c2c804225819c42a21805a7a7a8d752
VS <v@s>
2020-09-23 23:29:28 +0300

    + Github RepositoryFetcher

---
+++ lib/app/github/repository_fetcher.ex
@@ -0,0 +1,78 @@
+defmodule App.Github.RepositoryFetcher do
+  use TypedStruct
+  use Tesla
+
+  plug Tesla.Middleware.FollowRedirects, max_redirects: 1
+  plug Tesla.Middleware.BaseUrl, "https://api.github.com"
+  plug Tesla.Middleware.Headers, headers()
+  plug Tesla.Middleware.JSON
+
+  defmodule Repo do
+    typedstruct enforce: true do
+      field :id, integer()
+      field :full_name, String.t()
+      field :data, map()
+    end
+  end
+
+  defmodule Limit do
+    typedstruct enforce: true do
+      field :limit, integer()
+      field :remain, integer()
+      field :reset_at, integer()
+    end
+  end
+
+  defp headers do
+    default = [{"User-Agent", "Tesla"}]
+
+    case token = Application.get_env(:app, :github_api_token) do
+      nil -> default
+      _ -> default ++ [{"Authorization", "token #{token}"}]
+    end
+  end
+
+  @spec fetch(String.t()) ::
+          {:ok, Repo.t(), Limit.t()}
+          | {:error, :repo_private}
+          | {:error, :limit_exceeded, Limit.t()}
+          | {:error, :forbidden}
+          | {:error, Tesla.Env.t()}
+  def fetch(full_name) when is_binary(full_name) do
+    unless String.match?(full_name, ~r|[^/]+/[^/]+|),
+      do: raise(ArgumentError, message: "Argument should match the pattern \"{owner}/{repo}\"")
+
+    {:ok, github_response} = get("/repos/#{full_name}")
+
+    case github_response do
+      %Tesla.Env{status: 200} ->
+        {:ok, repo(github_response), limit(github_response)}
+
+      %Tesla.Env{status: 404} ->
+        {:error, :repo_private}
+
+      %Tesla.Env{status: 403} ->
+        case l = limit(github_response) do
+          %Limit{remain: 0} -> {:error, :limit_exceeded, l}
+          _ -> {:error, :forbidden}
+        end
+
+      _ ->
+        {:error, github_response}
+    end
+  end
+
+  defp repo(%Tesla.Env{status: 200, body: body}) do
+    %Repo{id: body["id"], full_name: body["full_name"], data: body}
+  end
+
+  defp limit(%Tesla.Env{headers: headers}) do
+    headers = Enum.into(headers, %{})
+
+    {limit, ""} = Integer.parse(headers["x-ratelimit-limit"])
+    {remain, ""} = Integer.parse(headers["x-ratelimit-remaining"])
+    {reset_at_unix, ""} = Integer.parse(headers["x-ratelimit-reset"])
+    reset_at = DateTime.from_unix!(reset_at_unix)
+    %Limit{limit: limit, remain: remain, reset_at: reset_at}
+  end
+end
---
+++ test/app/github/repository_fetcher_test.exs
@@ -0,0 +1,88 @@
+defmodule App.Github.RepositoryFetcherTest do
+  use ExUnit.Case
+  import Tesla.Mock
+  alias App.Github.RepositoryFetcher
+  alias App.Github.RepositoryFetcher.Repo
+  alias App.Github.RepositoryFetcher.Limit
+
+  test "retrieve repo info" do
+    mock(fn %{method: :get, url: "https://api.github.com/repos/foo/bar"} ->
+      %Tesla.Env{
+        status: 200,
+        body: %{"id" => 123, "full_name" => "foo/bar", "owner" => %{"login" => "foo"}},
+        headers: [
+          {"x-ratelimit-limit", "60"},
+          {"x-ratelimit-remaining", "59"},
+          {"x-ratelimit-reset", "1563085238"}
+        ]
+      }
+    end)
+
+    assert RepositoryFetcher.fetch("foo/bar") ==
+             {
+               :ok,
+               %Repo{
+                 id: 123,
+                 full_name: "foo/bar",
+                 data: %{"id" => 123, "full_name" => "foo/bar", "owner" => %{"login" => "foo"}}
+               },
+               %Limit{limit: 60, remain: 59, reset_at: ~U[2019-07-14 06:20:38Z]}
+             }
+  end
+
+  test "retrieve private repo" do
+    mock(fn %{method: :get, url: "https://api.github.com/repos/private/private"} ->
+      %Tesla.Env{
+        status: 404
+      }
+    end)
+
+    assert RepositoryFetcher.fetch("private/private") ==
+             {:error, :repo_private}
+  end
+
+  test "api limit exceeded" do
+    mock(fn %{method: :get, url: "https://api.github.com/repos/foo/bar"} ->
+      %Tesla.Env{
+        status: 403,
+        headers: [
+          {"x-ratelimit-limit", "60"},
+          {"x-ratelimit-remaining", "0"},
+          {"x-ratelimit-reset", "1574803073"}
+        ]
+      }
+    end)
+
+    assert RepositoryFetcher.fetch("foo/bar") ==
+             {:error, :limit_exceeded, %Limit{limit: 60, remain: 0, reset_at: ~U[2019-11-26 21:17:53Z]}}
+  end
+
+  test "api limit not exceeded but 403" do
+    mock(fn %{method: :get, url: "https://api.github.com/repos/foo/bar"} ->
+      %Tesla.Env{
+        status: 403,
+        headers: [
+          {"x-ratelimit-limit", "60"},
+          {"x-ratelimit-remaining", "59"},
+          {"x-ratelimit-reset", "1574803073"}
+        ]
+      }
+    end)
+
+    assert RepositoryFetcher.fetch("foo/bar") ==
+             {:error, :forbidden}
+  end
+
+  test "other http error" do
+    response = %Tesla.Env{status: 500}
+    mock(fn %{} -> response end)
+
+    assert RepositoryFetcher.fetch("foo/bar") == {:error, response}
+  end
+
+  test "incorrect argument" do
+    assert_raise ArgumentError, "Argument should match the pattern \"{owner}/{repo}\"", fn ->
+      RepositoryFetcher.fetch("foobar")
+    end
+  end
+end
