6800f71179b24d68ff9314db7f6873ff994cfcc3
VS <v@s>
2020-09-23 22:59:00 +0300

    .

--- mix.exs
+++ mix.exs
@@ -64,7 +64,8 @@ defmodule App.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      "ecto.migrate": ["ecto.migrate --log-sql"],
+      test: ["ecto.drop", "ecto.create", "ecto.migrate --quiet", "test"]
     ]
   end
 end
