8d81c0177474f11ef73ce3b4dc6c1412eedbc864
VS <v@s>
2020-09-28 01:45:31 +0300

    ~ Switch to releases

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -25,8 +25,8 @@ secret_key_base =
 
 config :app, AppWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
+    ip: {127, 0, 0, 1},
+    port: String.to_integer(System.get_env("PORT") || "4000")
   ],
   secret_key_base: secret_key_base
 
@@ -35,7 +35,7 @@ config :app, AppWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :app, AppWeb.Endpoint, server: true
+config :app, AppWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
---
+++ lib/app/release.ex
@@ -0,0 +1,30 @@
+defmodule App.Release do
+  @moduledoc """
+  Run migrations in release mode
+
+  Examples:
+    $ bin/app eval "App.Release.migrate"
+    
+    $ bin/app eval "App.Release.rollback"
+  """
+
+  @app :app
+
+  def migrate do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    load_app()
+
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp load_app, do: Application.load(@app)
+
+  defp repos, do: Application.fetch_env!(@app, :ecto_repos)
+end
